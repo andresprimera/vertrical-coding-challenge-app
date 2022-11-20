da40273f0e5479fee00e79b39facf94249ab2ddd
andresprimera <andresprimera@gmail.com>
2022-11-20 16:35:01 -0500

    Modifying loading animation to improve responsiveness

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -6,9 +6,9 @@ import styles from "./styles.module.css";
 export default function Loader() {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 1 }}
-      animate={{ opacity: 1, scale: 2 }}
-      transition={{ repeat: Infinity, duration: 0.7 }}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ repeat: Infinity, duration: 1 }}
     >
       <div className={styles.container}>
         <h3 className={styles.text}>{"Loading"}</h3>
